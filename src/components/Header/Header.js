a28c11891a68a3b87243f841f6e5ed3fd75f2fee
Shamimzz <shamimislam5091@gmail.com>
2021-10-20 23:33:43 +0600

    final

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,19 +7,22 @@ import useAuth from '../Hooks/useAuth';
 
 import { AiOutlineLogin } from "react-icons/ai";
 import { AiOutlineLogout } from "react-icons/ai";
-import { FaUserCircle } from "react-icons/fa";
+// import { FaUserCircle } from "react-icons/fa";
 
 
 const Header = () => {
 
    const {user, handleLogOut} = useAuth();
 
-  //  const proPic = () => {
-  //   if(user.photoURL){
-  //    user.photoURL;
-  //    }else{
-  //     <FaUserCircle />
-  //    }
+   const proPic = () => {
+    if(user.photoURL){
+      return <img className="profileImg" src={user.photoURL} alt="" srcset="" />
+     }
+     else{
+      return <img className="profileImg" src="https://cdn.icon-icons.com/icons2/1161/PNG/512/1487716857-user_81635.png" alt="" srcset=""/>
+     }
+    }
+
 
     return (
         <div>
@@ -38,7 +41,7 @@ const Header = () => {
                </Nav>  
                <Navbar.Text className="">
                   <a href="#login" className="p-2 name">{user?.email && user.displayName}</a>
-                  <span>{user?.email && <img className="profileImg" src={user.photoURL} alt=""/>}</span>
+                  <span>{user?.email && <img src={proPic} alt=""/>}</span>
                </Navbar.Text> 
                { !user.email ? 
                 <Nav.Link as={Link} to="/login#login" className="btn btn-info text-dark m-3"><AiOutlineLogin /> Log In</Nav.Link>
